aeb83465fbb391008f8c2acf82902d327127e852
ricardorosa <rjrosa@outlook.com>
2024-04-23 09:43:29 +0100

    game over functionality

--- getThatClown.py
+++ getThatClown.py
@@ -35,8 +35,8 @@ pygame.mixer.music.load("getThatClownAssets/ctcBackgroundMusic.wav")
 backgroundImage = pygame.image.load("getThatClownAssets/background.png")
 clownImage = pygame.image.load("getThatClownAssets/clown.png")
 
-# GAME FUNCTIONALITY
 
+# GAME FUNCTIONALITY
 
 def inputHandler():
     """Handles user input"""
@@ -47,31 +47,54 @@ def inputHandler():
             clickHandler(event.pos)
     return True
 
+
 def clickHandler(pos):
     """Handles mouse click events"""
-    mouseX, mouseY = pos
-    if clownRect.collidepoint(mouseX, mouseY):
-        clickSound.play()
-        increaseScore()
-        updateClownVelocity()
+    if gameOver:
+        restartGame()
     else:
-        missSound.play()
-        decreasePlayerLives()
+        mouseX, mouseY = pos
+        if clownRect.collidepoint(mouseX, mouseY):
+            clickSound.play()
+            increaseScore()
+            updateClownVelocity()
+        else:
+            missSound.play()
+            decreasePlayerLives()
+
 
 def increaseScore():
     """Increases the player's score"""
     global score
     score += 1
+    updateScoreText()
+
+
+def updateLivesText():
+    """Updates the player lives HUD text"""
+    global livesText
+    livesText = font.render("Lives: " + str(playerLives), True, yellow)
+
+
+def updateScoreText():
+    """Updates the score HUD text"""
+    global scoreText, livesText
+    scoreText = font.render("Score: " + str(score), True, yellow)
+
 
 def updateClownVelocity():
     """Increases the velocity of the clown"""
     global clownVelocity
     clownVelocity += clownAcceleration
 
+
 def decreasePlayerLives():
     """Decreases the player's lives"""
     global playerLives
     playerLives -= 1
+    updateLivesText()
+    if playerLives == 0:
+        gameOverScreen()
 
 
 def moveClown():
@@ -85,6 +108,7 @@ def moveClown():
     if clownRect.top <= 0 or clownRect.bottom >= windowHeight:
         clownDy = -1 * clownDy
 
+
 def updateScreen():
     """Updates the display"""
     screen.blit(backgroundImage, (0, 0))
@@ -92,7 +116,30 @@ def updateScreen():
     screen.blit(scoreText, (windowWidth - 200, 10))
     screen.blit(livesText, (windowWidth - 200, 50))
     screen.blit(clownImage, clownRect)
+    if gameOver:
+        screen.blit(gameOverText, gameOverRect)
+        screen.blit(continueText, continueRect)
+
     pygame.display.update()
+
+
+def restartGame():
+    """Restarts the game"""
+    global score, playerLives, gameOver
+    score = 0
+    playerLives = playerStartingLives
+    clownRect.center = (windowWidth // 2, windowHeight // 2)
+    clownVelocity = clownStartingVelocity
+    clownDx = random.choice([-1, 1])
+    clownDy = random.choice([-1, 1])
+    gameOver = False
+
+
+def gameOverScreen():
+    """Displays the game over screen"""
+    global gameOver
+    gameOver = True
+
 
 # Set initial game state
 score = 0
@@ -101,18 +148,26 @@ clownRect = clownImage.get_rect(center=(windowWidth // 2, windowHeight // 2))
 clownDx = random.choice([-1, 1])
 clownDy = random.choice([-1, 1])
 clownVelocity = clownStartingVelocity
+gameOver = False
 
 # Set initial HUD text
 titleText = font.render("Get that Clown", True, blue)
 scoreText = font.render("Score: " + str(score), True, yellow)
 livesText = font.render("Lives: " + str(playerLives), True, yellow)
 
+gameOverText = font.render("GAME OVER", True, blue, yellow)
+continueText = font.render("Click anywhere to play again", True, yellow, blue)
+
+gameOverRect = gameOverText.get_rect(center=(windowWidth // 2, windowHeight // 2))
+continueRect = continueText.get_rect(center=(windowWidth // 2, windowHeight // 2 + 64))
+
 # The main game loop
 pygame.mixer.music.play(-1, 0.0)
 running = True
 while running:
     running = inputHandler()
-    moveClown()
+    if not gameOver:
+        moveClown()
     updateScreen()
     clock.tick(fps)
 
